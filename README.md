5f526a1a1c18ea95e7076dcdc2979b2c1930df94
Taehyun Kim <taehyunzzz@snu.ac.kr>
2018-08-02 15:05:34 +0900

    upgraded blur.py to augment.py

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Here are some utility python codes that can help you label your pictures. These codes allows you to surround your object with visualized square lables and translate them into the appropriate form for running yolo. Below are simple descriptions of what each python code does.
 
 ## Code Descriptions
-### 01_main.py
+### 1. 01_main.py
 Executes the labeling program. 
 1. Requires a "class.txt" file that identifies class names in separate lines.
 2. Requires an "Examples/demo/" directory that stores 3 demo images(Don't know whether random images work).
@@ -13,30 +13,30 @@ Executes the labeling program.
 6. Delete inserted labels by selecting and hitting "Delete" to delete a selected label, or click "ClearAll" to delete all labels for a single image.
 7. Each visualization of label is stored in ".txt" format inside "Labels/{your directory number}/"
 
-### 02_removeEmptyFile.py
+### 2. 02_removeEmptyFile.py
 Removes redundant images with no label and empty label files.
 1. Specify the path to your images and label text files in code. Where to fix is self-evident.
 
-### 03_Labels2Formal.py
+### 3. 03_Labels2Formal.py
 Converts labels to appropriate form for feeding yolo network.
 1. Specify the path to your label text files and path to save the converted labels.
 
-### 04_trainTestList.py
+### 4. 04_trainTestList.py
 Creates a list of train set and test set in separate ".list" files. They are named "train.list" and "test.list"
 1. Specify the directory where your Label_formal files reside and where your images reside.
 2. Specify the percentage to make your test set. [0,100] The remainder will become the train set.
 
-### 05_Formal2Labels.py
+### 5. 05_Formal2Labels.py
 In case the original label files are lost, use this code to convert your Label_formal files back to the original label files.
 
-### blur.py
+### 6. augment.py
 Augment your images by adding blur to your images. 
 1. Specify blur type, kernel size (x,y), and percentage out of 100 that you want to blur your original images.
 2. Executing this code will not overwrite the original images but rather create new ones with a prefix added to the original title.
 
-### make_list_general.py
+### 7. make_list_general.py
 Create a shuffled list of all ".jpg" files resident in the current directory.
 
-### rename_general.py
+### 8. rename_general.py
 Rename files in current directory by replacing a certain expression to another.
 1. Modify replace( {src} , {dst} )
---
+++ augment.py
@@ -0,0 +1,191 @@
+import os
+import shutil
+import imutils
+import cv2
+from os import walk
+
+
+""""""""""""""" Settings """""""""""""""
+"""
+func
+0: normalized box filter blur
+1: Gaussian blur
+2: median blur
+3: Gaussian noise
+4: salt and pepper noise
+5: rotate 180 degrees (no flipping)
+"""
+func = 0
+
+"""
+x,y
+width and height of blur kernel
+"""
+x = 6
+y = 6
+
+"""
+percentage out of all files to augment
+"""
+percentage = 0.4
+
+"""
+sigmaX: standard deviation in x. 0 implies that sigmaX is calculated using kernel size.
+sigmaY: standard deviation in y. 0 implies that sigmaY is calculated using kernel size.
+"""
+sigmaX = 0
+sigmaY = 0
+
+""""""""""""""" Settings """""""""""""""
+
+input_path = "./"
+output_path = "./"
+
+text_name_list = []
+img_name_list = []
+for (dirpath, dirnames, f) in walk(input_path):
+	for name in f:
+		if name.endswith('.txt'):
+			text_name_list.append(name)
+		elif name.endswith('.png') or name.endswith('.jpg') or name.endswith('.jpeg'):
+			img_name_list.append(name)
+
+def add_gaussian_noise(img):
+    
+    row, col, _ = img.shape
+    
+    """"""""""""""" Settings """""""""""""""
+    """
+    Gaussian distribution parameters
+    alpha – weight of the first array elements.
+    beta – weight of the second array elements.
+    gamma – scalar added to each sum.
+    """
+    alpha = 0.5
+    beta = 0.5
+    gamma = 0
+    strength = 0.25
+    """"""""""""""" Settings """""""""""""""
+    
+    gaussian = np.random.random((row, col, 1)).astype(np.float32)
+    gaussian = np.concatenate((gaussian, gaussian, gaussian), axis = 2)
+    gaussian_img = cv2.addWeighted(img, alpha, strength * gaussian, beta, gamma, dtype=-1)
+
+    return gaussian_img
+
+def add_salt_pepper_noise(X_imgs):
+    # Need to produce a copy as to not modify the original image
+    X_imgs_copy = X_imgs.copy()
+    row, col, _ = X_imgs_copy.shape
+    
+    """"""""""""""" Settings """""""""""""""
+    salt_vs_pepper = 0.2
+    amount = 0.004
+    """"""""""""""" Settings """""""""""""""
+    
+    num_salt = np.ceil(amount * X_imgs_copy[0].size * salt_vs_pepper)
+    num_pepper = np.ceil(amount * X_imgs_copy[0].size * (1.0 - salt_vs_pepper))
+    
+    for X_img in X_imgs_copy:
+        # Add Salt noise
+        coords = [np.random.randint(0, i - 1, int(num_salt)) for i in X_img.shape]
+        X_img[coords[0], :] = 1
+
+        # Add Pepper noise
+        coords = [np.random.randint(0, i - 1, int(num_pepper)) for i in X_img.shape]
+        X_img[coords[0], :] = 0
+        
+    return X_imgs_copy
+
+
+list_len = len(img_name_list)
+rst_num = int(1/percentage)
+cnt = 0
+
+
+for img_name in img_name_list:
+              
+        if(cnt%rst_num == 0) :
+                
+                img_path = input_path + img_name
+                print("Input: " + img_path)
+                print("Name: " + img_name)
+                src = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
+                if src is None:
+                        print("Wrong file")
+                        continue
+                print("Success load img")
+                dst = src.copy()
+                
+                if func == 0:
+                        print("blur function is called\n")
+                        dst = cv2.blur(src,(x,y))
+                        out_img = output_path + "blur_" + img_name
+                        cv2.imwrite(out_img, dst)
+                        
+                elif func == 1:
+                        if (x % 2) == 0:
+                                x = int((x-1)/2)*2+1
+                        if (y % 2) == 0:
+                                y = int((y-1)/2)*2+1
+                        print("Gaussian blur function is called\n")
+                        
+                        dst = cv2.GaussianBlur(src, (x,y), sigmaX)
+                        out_img = output_path + "gblur_" + img_name
+                        cv2.imwrite(out_img, dst)
+                        
+                elif func == 2:
+                        print("median blur function is called\n")
+                        if (x % 2) == 0:
+                                x = int((x-1)/2)*2+1
+                        dst = cv2.medianBlur(src, x)
+                        out_img = output_path + "mblur_" + img_name
+                        cv2.imwrite(out_img, dst)
+                        
+                elif func == 3:
+                        print("add Guassian noise function is called\n")
+                        dst = add_gaussian_noise(src, x)
+                        out_img = output_path + "gn_" + img_name
+                        cv2.imwrite(out_img, dst)
+                        
+                elif func == 4:
+                        print("add salt and pepper noise function is called\n")
+                        dst = add_salt_pepper_noise(src, x)
+                        out_img = output_path + "spn_" + img_name
+                        cv2.imwrite(out_img, dst)
+
+                elif func == 5:
+                        print("rotate 180 degree function is called\n")
+                        dst = imutils.rotate_bound(src, 180)
+                        out_img = output_path + "rot_" + img_name
+                        cv2.imwrite(out_img, dst)
+
+                
+                txt_name = text_name_list[img_name_list.index(img_name)]
+                
+                if(func == 0):
+                        shutil.copyfile(input_path+txt_name, output_path+"blur_"+txt_name)
+                elif(func == 1):
+                        shutil.copyfile(input_path+txt_name, output_path+"gblur_"+txt_name)
+                elif(func == 2):
+                        shutil.copyfile(input_path+txt_name, output_path+"mblur_"+txt_name)
+                elif(func == 3):
+                        shutil.copyfile(input_path+txt_name, output_path+"gn_"+txt_name)
+                elif(func == 4):
+                        shutil.copyfile(input_path+txt_name, output_path+"spn_"+txt_name)
+                elif(func == 5):
+                        text = open(output_path+txt_name, 'r')
+                        rot_text = open(output_path+"rot_"+txt_name, 'w')
+
+                        for line in text.read().split("\n")
+                                elems = line.split()
+                                cls_idx = elems[0]
+                                center_x = float(elems[1])
+                                center_y = float(elems[2])
+                                wd = float(elems[3])
+                                ht = float(elems[4])
+                                rot_text.write("{0} {1} {2} {3} {4}".format("0",str(1-center_x),str(1-center_y),str(wd),str(ht)))
+                        text.close()
+                        rot_text.close()                       
+        
+        cnt = cnt + 1
