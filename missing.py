cbf301fb06642e20b4380df56fde132b8e901078
Taehyun Kim <taehyunzzz@snu.ac.kr>
2018-08-06 22:49:12 +0900

    print and make list of imgs/labels with missing labels/imgs

---
+++ missing.py
@@ -0,0 +1,48 @@
+from os import getcwd, walk
+
+txtlist=[]
+imglist=[]
+
+for (dirpath,dirname,f) in walk(getcwd()):
+	for name in f:
+		if name.endswith('.txt'):
+			txtlist.append(name)
+		elif name.endswith('.jpg'):
+			imglist.append(name)
+
+missingimg=[]
+missinglabel=[]
+cntimg=0
+cntlabel=0
+print('list of txt/img with missing pair')
+
+for img in imglist:
+	if img.split(".")[0]+".txt" not in txtlist:
+		missinglabel.append(img)
+		cntlabel += 1
+
+for txt in txtlist:
+	if txt.split(".")[0]+".jpg" not in imglist:
+		missinglimg.append(txt)
+		cntlabel += 1
+
+missingimg.sort()
+missinglabel.sort()
+
+missing = open("missing.list",'w')
+
+print "missing img: {0}".format(cntimg)
+missing.write('missing img: {0}'.format(cntimg))
+
+for name in missingimg:
+	print name
+	missing.write(name)
+
+print "missing label: {0}".format(cntlabel)
+missing.write('missing label: {0}'.format(cntlabel))
+
+for name in missinglabel:
+	print name
+	missing.write(name)
+
+missing.close()
