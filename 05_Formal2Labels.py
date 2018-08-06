a3d1ad045cd8bee9a8043d01aed3b46ec89dfcd1
skarlzz <41991284+skarlzz@users.noreply.github.com>
2018-08-06 15:21:20 +0900

    05 multiple formal2Labels
    "How to Use"  is same with old version

--- 05_Formal2Labels.py
+++ 05_Formal2Labels.py
@@ -5,25 +5,27 @@ import imutils
 import cv2
 
 import os
-from os import walk, getcwd
+import glob
+from os import getcwd
 from PIL import Image
 
 def restore_label(label_orig_path, label_orig, save_path):
     orig_file = open(label_orig_path+label_orig, "r")
     wr_file = open(save_path+label_orig,"w")
-    
-    for lines in orig_file.read().splitlines():
-        print(lines)
+    lines_list = orig_file.read().splitlines()
+    wr_file.write("{}".format(len(lines_list)))
+    for lines in lines_list:
+        wr_file.write("\n")
         # class name / center coord x,y / width, height
         elems = lines.split()
-        cls_idx = elems[0]
+        cls_idx = int(elems[0])
         # normalized by size of image coordinates
         center_x = float(elems[1])
         center_y = float(elems[2])
         wd = float(elems[3])
         ht = float(elems[4])
-        
-        wr_file.write("1\n{0} {1} {2} {3} {4}".format(class_name, str(int(img_wd*(center_x-0.5*wd))), str(int(img_ht*(center_y-0.5*ht))), str(int(img_wd*(center_x+0.5*wd))), str(int(img_ht*(center_y+0.5*ht)))))
+        wr_file.write("{0} {1} {2} {3} {4}".format(classes[cls_idx], str(int(img_wd*(center_x-0.5*wd))), str(int(img_ht*(center_y-0.5*ht))), str(int(img_wd*(center_x+0.5*wd))), str(int(img_ht*(center_y+0.5*ht)))))
+        #wr_file.write("{0}\n{1} {2} {3} {4} {5}".format(num, classes[cls_idx], str(int(img_wd*(center_x-0.5*wd))), str(int(img_ht*(center_y-0.5*ht))), str(int(img_wd*(center_x+0.5*wd))), str(int(img_ht*(center_y+0.5*ht)))))
         
     wr_file.close()
     
@@ -31,7 +33,7 @@ def restore_label(label_orig_path, label_orig, save_path):
     
 """"""""" Modify before use """""""""""
 # Set basic information of image and class
-class_name = "Staples"
+classes = ["ParisRedbean","Vita500","TunaCan","VitaJelly","Myzzo","Staples"]
 
 # Image width and height
 img_wd = 640
@@ -42,20 +44,12 @@ label_formal_path = "Labels_formal/001/"
 label_save_path = "Labels/001/"
 """"""""" Modify before use """""""""""
 
-
-classes = ["001"]
-cls = "001"
-if cls not in classes:
-    exit(0)
-cls_id = classes.index(cls)
-
 wd = getcwd()
     
 """ Get input label file list """
 label_name_list = []
-for (dirpath, dirnames, filenames) in walk(label_formal_path):
-    label_name_list.extend(filenames)
-    break
+for filename in glob.iglob(os.path.join(label_formal_path, "*.txt")):
+    label_name_list.append(os.path.basename(filename))
 
 """ Restore original label from formalized label """
 for label_formal_file in label_name_list :
